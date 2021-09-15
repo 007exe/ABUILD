0e44fdfb1a4172cc5c61639f4f65651197e298d9
007exe <AlekseiChip@yandex.ru>
2021-09-15 19:40:49 +0300

    Обновлён etc-20210908 Теперь все файлы в files, а не на git

--- base/etc/doinst.sh
+++ base/etc/doinst.sh
@@ -10,6 +10,30 @@ config() {
   fi
   # Otherwise, we leave the .new copy for the admin to consider...
 }
+
+# First, make sure any new entries in passwd/shadow/group are added:
+if [ -r etc/passwd -a -r etc/passwd.new ]; then
+  cat etc/passwd.new | while read line ; do
+    if ! grep -q "^$(echo $line | cut -f 1 -d :):" etc/passwd ; then
+      echo $line >> etc/passwd
+    fi
+  done
+fi
+if [ -r etc/shadow -a -r etc/shadow.new ]; then
+  cat etc/shadow.new | while read line ; do
+    if ! grep -q "^$(echo $line | cut -f 1 -d :):" etc/shadow ; then
+      echo $line >> etc/shadow
+    fi
+  done
+fi
+if [ -r etc/group -a -r etc/group.new ]; then
+  cat etc/group.new | while read line ; do
+    if ! grep -q "^$(echo $line | cut -f 1 -d :):" etc/group ; then
+      echo $line >> etc/group
+    fi
+  done
+fi
+
 config etc/mtab.new
 config etc/motd.new
 config etc/group.new
@@ -35,13 +59,15 @@ config var/log/lastlog.new
 config var/log/wtmp.new
 config var/run/utmp.new
 
-if [ -r etc/ld.so.conf.new ]; then
+if [ -r etc/ld.so.conf.new -a -r etc/ld.so.conf ]; then
   # Ensure that ld.so.conf contains the minimal set of paths:
-  cat etc/ld.so.conf.new | while read pathline ; do
-    if ! grep "$pathline" etc/ld.so.conf 1> /dev/null 2> /dev/null ; then
-      echo "$pathline" >> etc/ld.so.conf
+  # (eliminate ld.so.conf.d line when adding paths to avoid repeats)
+  cat etc/ld.so.conf | grep -v ld.so.conf.d | while read pathline ; do
+    if ! grep "^${pathline}$" etc/ld.so.conf.new 1> /dev/null 2> /dev/null ; then
+      echo "$pathline" >> etc/ld.so.conf.new
     fi
   done
+  cp etc/ld.so.conf.new etc/ld.so.conf
 fi
 
 # Clean up useless non-examples:
@@ -62,80 +88,14 @@ rm -f var/run/utmp.new
 rm -f var/log/lastlog.new
 rm -f var/log/wtmp.new
 
-# Making sure these are there will save us headaches:
-if ! grep -q "^dialout:" etc/group ; then
-  echo "dialout:x:16:" >> etc/group
-fi
-if ! grep -q "^audio:" etc/group ; then
-  echo "audio:x:17:root" >> etc/group
-fi
-if ! grep -q "^video:" etc/group ; then
-  echo "video:x:18:root" >> etc/group
-fi
-if ! grep -q "^cdrom:" etc/group ; then
-  echo "cdrom:x:19:root" >> etc/group
-fi
-if ! grep -q "^utmp:" etc/group ; then
-  echo "utmp:x:22:" >> etc/group
-fi
-if ! grep -q "^tape:" etc/group ; then
-  echo "tape:x:26:root" >> etc/group
-fi
-if ! grep -q "^shadow:" etc/group ; then
-  echo "shadow:x:43:" >> etc/group
-fi
-if ! grep -q "^oprofile:" etc/passwd ; then
-  echo "oprofile:x:51:51:oprofile:/:/bin/false" >> etc/passwd
-fi
-if ! grep -q "^oprofile:" etc/group ; then
-  echo "oprofile:x:51:" >> etc/group
-fi
-if ! grep -q "^apache:" etc/passwd ; then
-  echo "apache:x:80:80:User for Apache:/srv/httpd:/bin/false" >> etc/passwd
-fi
-if ! grep -q "^apache:" etc/group ; then
-  echo "apache:x:80:" >> etc/group
-fi
-if ! grep -q "^messagebus:" etc/passwd ; then
-  echo "messagebus:x:81:81:User for D-BUS:/var/run/dbus:/bin/false" >> etc/passwd
-fi
-if ! grep -q "^messagebus:" etc/group ; then
-  echo "messagebus:x:81:" >> etc/group
-fi
-if ! grep -q "^haldaemon:" etc/passwd ; then
-  echo "haldaemon:x:82:82:User for HAL:/var/run/hald:/bin/false" >> etc/passwd
-fi
-if ! grep -q "^haldaemon:" etc/group ; then
-  echo "haldaemon:x:82:" >> etc/group
-fi
-if ! grep -q "^plugdev:" etc/group ; then
-  echo "plugdev:x:83:root" >> etc/group
-fi
-if ! grep -q "^power:" etc/group ; then
-  echo "power:x:84:root" >> etc/group
-fi
-if ! grep -q "^netdev:" etc/group ; then
-  echo "netdev:x:86:root" >> etc/group
-fi
-if ! grep -q "^scanner:" etc/group ; then
-  echo "scanner:x:93:root" >> etc/group
-fi
+# Make sure $HOME is correct for user sddm:
+chroot . /usr/sbin/usermod -d /var/lib/sddm sddm > /dev/null 2> /dev/null
+# Make sure that sddm is a member of group video:
+chroot . /usr/sbin/usermod --groups video sddm > /dev/null 2> /dev/null
 
 # Also ensure ownerships/perms:
 chown root.utmp var/run/utmp var/log/wtmp
 chmod 664 var/run/utmp var/log/wtmp
 chown root.shadow etc/shadow etc/gshadow
 chmod 640 etc/shadow etc/gshadow
-
-if [ ! -r etc/termcap ]; then
- cp -a etc/termcap-Linux etc/termcap
-fi
-
-# I don't know why this was ever here, so I better
-# leave it, but comment it out
-#if [ ! -r etc/passwd.OLD ]; then
-#  mv etc/npasswd etc/passwd.OLD
-#else
-#  rm etc/npasswd
-#fi
 
