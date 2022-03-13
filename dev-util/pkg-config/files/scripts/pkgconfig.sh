05f78efbdc9bc56b0d045012fd23f0b19adc99b6
007exe <AlekseiChip@yandex.ru>
2022-03-13 14:20:43 +0300

    pkg-config заменён на pkgconf

--- dev-util/pkg-config/files/scripts/pkgconfig.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-if [ ! "$PKG_CONFIG_PATH" = "" ]; then
-  PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:/usr/local/lib/pkgconfig
-else
-  PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:/usr/lib/pkgconfig
-fi
-export PKG_CONFIG_PATH
