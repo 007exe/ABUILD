3c96edd1173f24a460d7618a7255ce1d2ceb28bd
007exe <AlekseiChip@yandex.ru>
2022-03-20 15:29:07 +0300

    Обновлён пакет syslinux-6.04_pre1-3, перенос библиотек в /usr/lib

--- sys-boot/syslinux/doinst.sh
+++ sys-boot/syslinux/doinst.sh
@@ -1,20 +1,15 @@
-echo " "
-echo "==> If you want to use syslinux as your bootloader"
-echo "==> edit /boot/syslinux/syslinux.cfg and run"
-echo "==>   # /usr/bin/syslinux-install_update -i -a -m"
-echo "==> to install it."
+echo -e "\n"
+echo -e "syslinux package note:"
+echo -e "To use syslinux as the default bootloader, run /usr/bin/syslinux-install_update -i -a -m and edit /boot/syslinux/syslinux.cfg"
+echo -e "The following packages extend syslinux capabilities:"
+echo -e "perl-crypt-passwdmd5: For md5pass"
+echo -e "perl-digest-sha1: For sha1pass"
+echo -e "mtools: For mkdiskimage and syslinux support"
+echo -e "gptfdisk: For GPT support"
+echo -e "util-linux: For isohybrid"
+echo -e "efibootmgr: For EFI support"
+echo -e "dosfstools: For EFI support"
+echo -e "\n"
 
-# auto-update syslinux if /boot/syslinux/SYSLINUX_AUTOUPDATE exists
-/usr/bin/syslinux-install_update -s
-# update to 5.10 message
-echo "If you used syslinux-install_update to install syslinux:"
-echo "==> If you want to use syslinux with menu and all modules please rerun" 
-echo "==>   # /usr/bin/syslinux-install_update -i -a -m"
-echo ""
-echo "If you manually installed syslinux:"
-echo "==> Please copy or symlink all .c32 modules to your /boot/syslinux directory."
-echo "==> If (/ and /boot on seperate fs):"
-echo "==>   # cp /usr/lib/syslinux/*.c32 /boot/syslinux"
-echo "==> If (/ and /boot on same fs):"
-echo "==>   # ln -s /usr/lib/syslinux/*.c32 /boot/syslinux"
-echo " "
+# Авто обновление syslinux
+usr/bin/syslinux-install_update -s
