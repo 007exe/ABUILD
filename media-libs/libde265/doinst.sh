a3419ddd2f4915908bf9d58fc7c56ccfaaf3a476
007exe <AlekseiChip@yandex.ru>
2022-03-25 21:01:51 +0300

    Обновлён пакет libde265-1.0.8-2, перенос библиотек в /usr/lib, изменёна категория

---
+++ media-libs/libde265/doinst.sh
@@ -0,0 +1,6 @@
+echo -e "\n"
+echo -e "The following packages extend libde265 capabilities:"
+echo -e "ffmpeg: for sherlock265"
+echo -e "qt5-base: for sherlock265"
+echo -e "sdl: dec265 YUV overlay output"
+
