c7415d5424ae81334c1fca0a7f2a1c3a5e5a1238
007exe <AlekseiChip@yandex.ru>
2022-03-27 11:12:31 +0300

    Обновлён пакет opam-2.1.2, перенос библиотек в /usr/lib

---
+++ dev-ml/opam/doinst.sh
@@ -0,0 +1,6 @@
+echo -e "\n"
+echo -e "The following packages extend opam capabilities:"
+echo -e "darcs: For downloading packages with darcs"
+echo -e "git: For downloading packages with git"
+echo -e "mercurial: For downloading packages with mercurial"
+echo -e "rsync: For downloading packages with rsync"
