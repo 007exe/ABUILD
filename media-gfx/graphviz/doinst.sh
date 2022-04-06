c86a4da110751b783fedd917923d4383ce4b7b4e
007exe <AlekseiChip@yandex.ru>
2022-04-06 18:13:22 +0300

    Обновлён пакет graphviz-3.0.0, перенос библиотек в /usr/lib

--- media-gfx/graphviz/doinst.sh
+++ media-gfx/graphviz/doinst.sh
@@ -1,1 +1,17 @@
-dot -c
+echo -e "\n"
+echo -e "The following packages extend graphviz capabilities:"
+echo -e "mono: sharp bindings"
+echo -e "guile: guile bindings"
+echo -e "lua: lua bindings"
+echo -e "ocaml: ocaml bindings"
+echo -e "perl: perl bindings"
+echo -e "python: python bindings"
+echo -e "r: r bindings"
+echo -e "tcl: tcl bindings"
+echo -e "qt5-base: gvedit"
+echo -e "gtk2: gtk output plugin"
+echo -e "xterm: vimdot"
+echo -e ""
+
+rm -f usr/lib/graphviz/config{,6}
+usr/bin/dot -c
