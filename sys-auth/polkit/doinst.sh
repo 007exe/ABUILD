3a2a9861681c7e01d7542adefdd88bc83685c5e4
007exe <AlekseiChip@yandex.ru>
2022-04-15 18:46:43 +0300

    Обновлён пакет polkit-0.120-2, перенос библиотек в /usr/lib

--- sys-auth/polkit/doinst.sh
+++
@@ -1,8 +0,0 @@
-# Make sure the polkitd user and group exist:
-if ! grep -q "^polkitd:" etc/passwd ; then
-  echo "polkitd:x:87:87:PolicyKit daemon owner:/var/lib/polkit:/bin/false" >> etc/passwd
-fi
-if ! grep -q "^polkitd:" etc/group ; then
-  echo "polkitd:x:87:" >> etc/group
-fi
-
