c1e9cb09de72062c3bbd254fae06c88a242eec1a
007exe <AlekseiChip@yandex.ru>
2022-04-24 08:36:43 +0300

    Обновлён пакет ffmpeg-5.0.1, перенос библиотек в /usr/lib

---
+++ media-video/ffmpeg/doinst.sh
@@ -0,0 +1,7 @@
+echo -e "\n"
+echo -e "The following packages extend ffmpeg capabilities:"
+echo -e "avisynthplus: AviSynthPlus support"
+echo -e "intel-media-sdk: Intel QuickSync support"
+echo -e "ladspa: LADSPA filters"
+echo -e "nvidia-utils: Nvidia NVDEC/NVENC support"
+echo -e " "
