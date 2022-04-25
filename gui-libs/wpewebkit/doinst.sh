6ff4e46acfdcb0eff7c7843ef83a5e2c9c81524f
007exe <AlekseiChip@yandex.ru>
2022-04-25 19:06:12 +0300

    Добавлен пакет wpewebkit

---
+++ gui-libs/wpewebkit/doinst.sh
@@ -0,0 +1,7 @@
+echo -e "\n"
+echo -e "The following packages extend wpewebkit capabilities:"
+echo -e "geoclue: Geolocation support"
+echo -e "gst-plugins-good: media decoding"
+echo -e "gst-plugins-bad: media decoding"
+echo -e "gst-libav: nonfree media decoding"
+echo -e " "
