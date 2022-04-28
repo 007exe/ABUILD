3799a1e32fcc90a4061861d8230202c5318c07e7
007exe <AlekseiChip@yandex.ru>
2022-04-28 21:29:45 +0300

    Обновлён пакет openal-1.22.0, перенос библиотек в /usr/lib

---
+++ media-libs/openal/doinst.sh
@@ -0,0 +1,6 @@
+echo -e "\n"
+echo -e "The following packages extend openal capabilities:"
+echo -e "qt5-base: alsoft-config GUI Configurator"
+echo -e "fluidsynth: MIDI rendering"
+echo -e "libmysofa: makemhr tool"
+echo -e " "
