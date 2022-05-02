b4db01c37f03180b2880da6347151a548cd284c4
007exe <AlekseiChip@yandex.ru>
2022-05-02 22:27:18 +0300

    Обновлён пакет lxde-common-0.99.2-2, перенос библиотек в /usr/lib

---
+++ lxde-base/lxde-common/doinst.sh
@@ -0,0 +1,5 @@
+echo -e "\n"
+echo -e "The following packages extend lxde-common capabilities:"
+echo -e "gnome-themes-standard: default GTK+ theme"
+echo -e "lxde-icon-theme: default icon theme"
+echo -e " "
