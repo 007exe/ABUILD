4b4ced9e2da26b1e5553d6f15199a51ff4ced5c4
007exe <AlekseiChip@yandex.ru>
2022-05-06 22:35:23 +0300

    Добавлен пакет perl-xml-libxml

--- dev-perl/perl-xml-libxml/doinst.sh
+++ dev-perl/perl-xml-libxml/doinst.sh
@@ -1,4 +1,4 @@
-libxmlcount=`grep -c XML::LibXML::SAX /usr/lib64/perl5/vendor_perl/XML/SAX/ParserDetails.ini`
+libxmlcount=`grep -c XML::LibXML::SAX /usr/lib/perl5/vendor_perl/XML/SAX/ParserDetails.ini`
 if [[ $libxmlcount == 0 ]]; then
     echo ":: Installing SAX XML Parsers"
     /usr/bin/perl -MXML::SAX -e "XML::SAX->add_parser(q(XML::LibXML::SAX::Parser))->save_parsers()"
