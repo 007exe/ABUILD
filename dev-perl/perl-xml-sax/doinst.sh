393e54ead233084ae39796d1d88d46ddb021b698
007exe <AlekseiChip@yandex.ru>
2022-05-06 22:30:30 +0300

    Удалены пакеты которые теперь предоставляет perl

--- dev-perl/perl-xml-sax/doinst.sh
+++ dev-perl/perl-xml-sax/doinst.sh
@@ -1,3 +1,3 @@
-if [ ! -r usr/share/perl5/vendor_perl/XML/SAX/ParserDetails.ini ]; then 
+if [ ! -r usr/share/perl5/vendor_perl/XML/SAX/ParserDetails.ini ]; then
 	perl -MXML::SAX -e "XML::SAX->add_parser(q(XML::SAX::PurePerl))->save_parsers()" &>/dev/null
 fi
