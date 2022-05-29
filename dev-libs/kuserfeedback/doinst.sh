9c1afa63fcdfd9176445a1462e7143a2f6d45501
007exe <AlekseiChip@yandex.ru>
2022-05-29 21:40:25 +0300

    Добавлен пакет kuserfeedback

---
+++ dev-libs/kuserfeedback/doinst.sh
@@ -0,0 +1,6 @@
+echo -e "\n"
+echo -e "The following packages extend kuserfeedback capabilities:"
+echo -e "qt5-declarative: QML bindings"
+echo -e "qt5-charts: User Feedback console"
+echo -e "qt5-svg: User Feedback console"
+echo -e " "
