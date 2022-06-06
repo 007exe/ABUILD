2cbc7f0e598b9b4f540c997a5d5750ab2e625f35
007exe <AlekseiChip@yandex.ru>
2022-06-06 18:53:43 +0300

    Добавлен пакет mate-settings-daemon

---
+++ mate-base/mate-settings-daemon/doinst.sh
@@ -0,0 +1,5 @@
+echo -e "\n"
+echo -e "The following packages extend mate-settings-daemon capabilities:"
+echo -e "libcanberra-pulse: PulseAudio support"
+echo -e "pulseaudio-alsa: PulseAudio support"
+echo -e " "
