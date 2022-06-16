15a10090c9f78c29603aa88f04cd1394b8d871b8
007exe <AlekseiChip@yandex.ru>
2022-06-16 19:11:48 +0300

    Обновлён пакет speech-dispatcher-0.11.1-2, перенос библиотек в /usr/lib

---
+++ app-accessibility/speech-dispatcher/doinst.sh
@@ -0,0 +1,5 @@
+echo -e "\nThe following packages extend speech-dispatcher capabilities:"
+echo -e "festival: Speech output using Festival"
+echo -e "espeak-ng: Speech output using ESpeak-ng"
+echo -e "pulseaudio: PulseAudio support"
+echo -e " "
