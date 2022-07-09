0a3cd8bee9ab9b669faa0f8f4f2615bb39044652
007exe <AlekseiChip@yandex.ru>
2022-07-09 13:42:01 +0300

    Добавлен пакет rpm-4.17.1

--- app-arch/rpm/doinst.sh
+++ app-arch/rpm/doinst.sh
@@ -5,3 +5,8 @@ else
     echo "No RPM database found... Creating database..."
     /usr/bin/rpmdb --initdb --root=/
 fi
+
+echo "    rpm installs RedHat package manager for you."
+echo "    It is useful for those who wants to create/modify RPM files."
+echo "    But don't use rpm to install Red Hat packages on your computer."
+echo "    It will break your system!"
