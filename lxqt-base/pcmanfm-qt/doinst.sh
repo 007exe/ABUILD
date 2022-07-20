24450435813f1fbb86df300372be035626a66655
007exe <AlekseiChip@yandex.ru>
2022-07-20 20:47:39 +0300

    Ребилд pcmanfm-qt, добавлена зависимость gvfs

--- lxqt-base/pcmanfm-qt/doinst.sh
+++ lxqt-base/pcmanfm-qt/doinst.sh
@@ -1,6 +1,5 @@
 echo -e "\n"
 echo -e "The following packages extend pcmanfm-qt capabilities:"
-echo -e "gvfs: for various features like mounting without root, remote filesystems, encrypted filesystems and many others mentioned on https://github.com/lxqt/pcmanfm-qt/wiki"
 echo -e "gvfs-mtp: MTP support (Media Transfer Protocol)"
 echo -e "lxqt-archiver: archive management"
 echo -e " "
