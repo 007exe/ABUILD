6fabb45de1349ddca37175a25165774ad83e19e2
007exe <AlekseiChip@yandex.ru>
2022-07-24 20:03:19 +0300

    Добавлен пакет pinentry

---
+++ app-crypt/pinentry/doinst.sh
@@ -0,0 +1,6 @@
+echo -e "\n"
+echo -e "The following packages extend pinentry capabilities:"
+echo -e "gtk2: gtk2 backend"
+echo -e "qt5-base: qt backend"
+echo -e "gcr: gnome3 backend"
+echo -e " "
