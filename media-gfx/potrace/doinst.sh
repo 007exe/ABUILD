e5771421a5caa77e879d710267b8e25f016613f9
007exe <AlekseiChip@yandex.ru>
2022-07-24 19:42:30 +0300

    Добавлен пакет potrace

---
+++ media-gfx/potrace/doinst.sh
@@ -0,0 +1,8 @@
+echo -e "\n"
+echo -e "The following packages extend inkscape capabilities:"
+echo -e "fig2dev: xfig input"
+echo -e "gvfs: import clip art"
+echo -e "pstoedit: latex formulas"
+echo -e "scour: optimized SVG output, some extensions"
+echo -e "texlive-core: latex formulas"
+echo -e " "
