31acc41cd6b4697bec91a0f14effe480c5d39b5f
007exe <AlekseiChip@yandex.ru>
2022-08-12 17:51:03 +0300

    Обновлён пакет rsync-3.2.5pre2

--- net-misc/rsync/files/rsyncd.init.d
+++ net-misc/rsync/files/rsyncd.init.d
@@ -1,23 +1,9 @@
-#!/sbin/openrc-run
-# Copyright 1999-2004 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/net-misc/rsync/files/rsyncd.init.d,v 1.5 2007/02/23 11:33:59 uberlord Exp $
+#!/usr/bin/openrc-run
+
+command="/usr/bin/rsync"
+command_args="--daemon ${RSYNC_OPTS}"
+pidfile="/var/run/${SVCNAME}.pid"
 
 depend() {
 	use net
-}
-
-start() {
-	ebegin "Starting rsyncd"
-	start-stop-daemon --start --exec /usr/bin/rsync \
-	    --pidfile /var/run/rsyncd.pid \
-	    -- --daemon ${RSYNC_OPTS}
-	eend $?
-}
-
-stop() {
-	ebegin "Stopping rsyncd"
-	start-stop-daemon --stop --exec /usr/bin/rsync \
-	    --pidfile /var/run/rsyncd.pid
-	eend $?
 }
