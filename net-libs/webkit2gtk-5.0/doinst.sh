572950148f546e6853a0394121aaee06d52cf606
007exe <AlekseiChip@yandex.ru>
2022-08-16 19:11:47 +0300

    Добавлен пакет webkit2gtk-5.0

---
+++ net-libs/webkit2gtk-5.0/doinst.sh
@@ -0,0 +1,7 @@
+echo -e "\n"
+echo -e "The following packages extend webkit2gtk-5.0 capabilities:"
+echo -e "geoclue: Geolocation support"
+echo -e "gst-plugins-good: media decoding"
+echo -e "gst-plugins-bad: media decoding"
+echo -e "gst-libav: nonfree media decoding"
+echo -e " "
