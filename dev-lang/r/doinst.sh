118771ac9cc001b904e2bb3269c0383b19531f08
007exe <AlekseiChip@yandex.ru>
2022-08-20 11:22:51 +0300

    Добавлен пакет r

---
+++ dev-lang/r/doinst.sh
@@ -0,0 +1,7 @@
+echo -e "\n"
+echo -e "The following packages extend R capabilities:"
+echo -e "tk: tcl/tk interface"
+echo -e "texlive-bin: latex sty files"
+echo -e "gcc-fortran: needed to compile some CRAN packages"
+echo -e "openblas: faster linear algebra"
+echo -e " "
