a175f68ad0d28fb93bdac4fb5106bebf607bb553
007exe <AlekseiChip@yandex.ru>
2022-11-04 23:27:19 +0300

    Обновлён пакет docbook-xml-4.5-3

--- app-text/docbook-xml/doinst.sh
+++ app-text/docbook-xml/doinst.sh
@@ -1,22 +1,21 @@
 if [ -e etc/xml/catalog.preserve ]; then
-	mv etc/xml/catalog.preserve etc/xml/catalog
+  mv etc/xml/catalog.preserve etc/xml/catalog
 elif [ ! -e etc/xml/catalog ]; then
-	xmlcatalog --noout --create etc/xml/catalog
+  xmlcatalog --noout --create etc/xml/catalog
 fi
-xmlcatalog --create --noout --add "delegatePublic" \
-	"-//OASIS//ENTITIES DocBook XML" \
-	"file:///etc/xml/docbook-xml" \
-	etc/xml/catalog
-xmlcatalog --create --noout --add "delegatePublic" \
-	"-//OASIS//DTD DocBook XML" \
-	"file:///etc/xml/docbook-xml" \
-	etc/xml/catalog
-xmlcatalog --create --noout --add "delegateSystem" \
-	"http://www.oasis-open.org/docbook/" \
-	"file:///etc/xml/docbook-xml" \
-	etc/xml/catalog
-xmlcatalog --create --noout --add "delegateURI" \
-	"http://www.oasis-open.org/docbook/" \
-	"file:///etc/xml/docbook-xml" \
-	etc/xml/catalog
-
+xmlcatalog --noout --add "delegatePublic" \
+  "-//OASIS//ENTITIES DocBook XML" \
+  "file:///etc/xml/docbook-xml" \
+  etc/xml/catalog
+xmlcatalog --noout --add "delegatePublic" \
+  "-//OASIS//DTD DocBook XML" \
+  "file:///etc/xml/docbook-xml" \
+  etc/xml/catalog
+xmlcatalog --noout --add "delegateSystem" \
+  "http://www.oasis-open.org/docbook/" \
+  "file:///etc/xml/docbook-xml" \
+  etc/xml/catalog
+xmlcatalog --noout --add "delegateURI" \
+  "http://www.oasis-open.org/docbook/" \
+  "file:///etc/xml/docbook-xml" \
+  etc/xml/catalog
