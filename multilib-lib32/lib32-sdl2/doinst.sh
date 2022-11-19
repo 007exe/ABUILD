efe080b55152c858ca3ad5310fdd6543be61fddb
007exe <AlekseiChip@yandex.ru>
2022-11-19 17:09:42 +0300

    Добавлен пакет lib32-sdl2

---
+++ multilib-lib32/lib32-sdl2/doinst.sh
@@ -0,0 +1,7 @@
+echo -e "\n"
+echo -e "The following packages extend lib32-sdl2 capabilities:"
+echo -e "lib32-alsa-lib: ALSA audio driver"
+echo -e "lib32-jack: JACK audio driver"
+echo -e "lib32-libpulse: PulseAudio audio driver"
+echo -e "lib32-pipewire: PipeWire audio driver"
+echo -e " "
