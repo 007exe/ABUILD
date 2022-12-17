7dd0748aae29fabfd35df798ae5dc69d627cd18d
007exe <AlekseiChip@yandex.ru>
2022-12-17 09:49:13 +0300

    Удалены устаревшие пакеты

--- _abulds-olds_/virtualbox-extpack/files/doinst.sh
+++
@@ -1,6 +0,0 @@
-GRP=`cat etc/group | grep vboxusers | head -n 1`
-if [ "$GRP" = "" ] ; then
-	chroot . groupadd vboxusers
-fi
-
-udevadm control --reload-rules
--- _abulds-olds_/virtualbox-extpack/files/extpack.sh
+++
@@ -1,1 +0,0 @@
-VBoxManage extpack install /usr/share/virtualbox/extensions/Oracle_VM_VirtualBox_Extension_Pack.vbox-extpack --replace
