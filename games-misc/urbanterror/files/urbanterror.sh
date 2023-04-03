0639d1bcff77cac70bfe1c790470c8f05b72bbb6
007exe <AlekseiChip@yandex.ru>
2023-04-03 22:22:27 +0300

    Обновлён пакет urbanterror-4.3.4

--- games-misc/urbanterror/files/urbanterror.sh
+++ games-misc/urbanterror/files/urbanterror.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-# cleanup mess, i did in last release
-if [ -d ~/.urbanterror/baseut4 ]; then
-	mkdir -p ~/.urbanterror/q3ut4
-	mv ~/.urbanterror/baseut4/*  ~/.urbanterror/q3ut4/
-	rmdir ~/.urbanterror/baseut4
-fi
-# start urbanterror
-exec ioUrbanTerror +set fs_basepath "/usr/share/urbanterror"
 
+cd /opt/urbanterror/
+exec ./urbanterror "$@"
