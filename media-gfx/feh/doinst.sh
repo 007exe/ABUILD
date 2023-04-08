5d0ff834c2d3b137d30ea4751b4b9556ebf695a0
007exe <AlekseiChip@yandex.ru>
2023-04-08 20:20:34 +0300

    Обновлён пакет feh-3.10

---
+++ media-gfx/feh/doinst.sh
@@ -0,0 +1,5 @@
+echo -e " "
+echo -e "The following packages extend feh capabilities:"
+echo -e "imagemagick: support more file formats"
+echo -e "jpegexiforient: set exif rotation tag"
+echo -e " "
