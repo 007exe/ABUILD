8ca84b4efdb600c6bc530730e8b0d9c969035d7a
007exe <AlekseiChip@yandex.ru>
2023-07-29 18:16:18 +0000

    Добавленна заготовка пакета connman

---
+++ net-misc/connman/doinst.sh
@@ -0,0 +1,8 @@
+echo -e " "
+echo -e "The following packages extend connman capabilities:"
+echo -e "bluez: Support for Bluetooth devices"
+echo -e "wpa_supplicant: for WiFi devices"
+echo -e "pptpclient: for ppp support"
+echo -e "openvpn: for VPN Support"
+echo -e "iwd: for WiFi devices"
+echo -e " "
