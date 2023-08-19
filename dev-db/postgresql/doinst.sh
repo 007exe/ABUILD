0a0c511d41f1180906f6e9314c0a9a68f4931c4d
007exe <AlekseiChip@yandex.ru>
2023-08-19 18:28:31 +0000

    Обновлён пакет postgresql-15.4

--- dev-db/postgresql/doinst.sh
+++ dev-db/postgresql/doinst.sh
@@ -1,24 +1,8 @@
-# Добавить пользователя и группу
-if ! grep -q "^pgsql:" etc/group; then
-    if ! grep -q ":28:" etc/group; then
-	chroot . groupadd -g 28 pgsql &>/dev/null
-    else
-	chroot . groupadd pgsql &>/dev/null
-    fi
-fi
-
-if ! grep -q "^pgsql:" etc/passwd; then
-    if ! grep -q ":28:" etc/passwd; then
-	chroot . useradd -u 28 -d /var/lib/pgsql -s /bin/false -c "PostgreSQL" -g 28 pgsql &>/dev/null
-    else
-	chroot . useradd -d /var/lib/pgsql -s /bin/false -c "PostgreSQL" -g 28 pgsql &>/dev/null
-    fi
-fi
-
-chown -R pgsql:pgsql var/log/pgsql
-chown pgsql:pgsql var/lib/pgsql
-
-# Выводит предупреждающее сообщение, если версия базы данных на диске не соответствует основной версии PostgreSQL.
-  if [ -d /var/lib/postgres/data ]; then
-	/usr/bin/postgresql-check-db-dir /var/lib/postgres/data || true
-  fi
+echo -e " "
+echo -e "The following packages extend postgresql capabilities:"
+echo -e "python: for PL/Python 3 support"
+echo -e "perl: for PL/Perl support"
+echo -e "tcl: for PL/Tcl support"
+echo -e "postgresql-old-upgrade: upgrade from previous major version using pg_upgrade"
+echo -e "logrotate: rotates system logs automatically"
+echo -e " "
