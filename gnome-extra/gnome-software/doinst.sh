83dd368b11a887e57c47e74787e8d35e0af16a7c
007exe <AlekseiChip@yandex.ru>
2023-11-11 11:45:16 +0000

    Добавлен пакет gnome-software

---
+++ gnome-extra/gnome-software/doinst.sh
@@ -0,0 +1,6 @@
+echo -e "\n"
+echo -e "The following packages extend gnome-software capabilities:"
+echo -e "    flatpak: Flatpak support plugin"
+echo -e "    fwupd: fwupd support plugin"
+echo -e "    malcontent: Parental control plugin"
+echo -e " "
